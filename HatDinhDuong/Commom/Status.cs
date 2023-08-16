8d9aa42b1f77b116dc78c4e3d19e8b7967de937d
linhld <baclinh0123@gmail.com>
2023-08-16 08:37:24 +0700

    init

---
+++ HatDinhDuong/Commom/Status.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HatDinhDuong.Commom
+{
+    public enum Status
+    {
+        [Display(Name = "Không hoạt động")]
+        InActive,
+        [Display(Name = "Hoạt động")]
+        Active
+    }
+}
---
+++ HatDinhDuong/Data/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace HatDinhDuong.Data.Entities
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Details { set; get; }
+        public string Customer { set; get; }
+        public string Localtion { set; get; }
+        public double Area { get; set; }
+        public DateTime DateCreated { set; get; }
+        public bool IsFeatured { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+        public List<ProductImage> ProductImages { get; set; }
+    }
+}
