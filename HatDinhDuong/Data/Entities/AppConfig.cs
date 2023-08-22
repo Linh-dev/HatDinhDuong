05e0e43b5d88e21d79148f0048ac83449fa08c38
LinhLD <linhld.ligosoft@gmail.com>
2023-08-23 00:29:06 +0700

    delete code

--- HatDinhDuong/Data/Entities/AppConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HatDinhDuong.Data.Entities
-{
-    public class AppConfig
-    {
-        public string Key { get; set; }
-        public string Value { get; set; }
-    }
-}
--- HatDinhDuong/Models/CloudinarySettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HatDinhDuong.Models
-{
-    public class CloudinarySettings
-    {
-        public string CouldName { get; set; }
-        public string ApiKey { get; set; }
-        public string ApiSecret { get; set; }
-    }
-}
